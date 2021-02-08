478ee0a6be31739d6b8a72c424d9d6630c44e2ab
Vicky Loebel <vickyloebel@gmail.com>
2021-02-07 22:48:08 -0700

    generate web page

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-};
+const [usr, github] = profileDataArgs;
+// desctructuring is the equivalent of
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-printProfileData(profileDataArgs);
+
+
+fs.writeFile('index.html', generatePage(usr, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
